251128e5361fb943576322a8a21a57aae5e78764
Max Rupnik <maxirupnik@gmail.com>
2025-04-16 15:36:12 +0200

    changed scene

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate sdl2;
-use scene::{Scene, Sphere, Material, ColorType, Light};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -9,17 +7,18 @@ mod vectors;
 use vectors::Vector;
 
 mod scene;
+use scene::{Scene, Sphere, Material, ColorType, Light};
 
 mod camera;
 use camera::Camera;
 
 const WIDTH: u16 = 1400;
-const RATIO: (u16, u16) = (16, 10);
+const ASPECT_RATIO: (u16, u16) = (16, 10);
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video = sdl_context.video()?;
-    let (x_ratio, y_ratio) = RATIO;
+    let (x_ratio, y_ratio) = ASPECT_RATIO;
     let height = (WIDTH as f64 * (y_ratio as f64 / x_ratio as f64)) as u32;
     let window = video
         .window("Ray Tracing in SDL2", WIDTH as u32, height)
@@ -38,17 +37,17 @@ fn main() -> Result<(), String> {
             },
             radius: 1.0,
             material: Material {
-                color: ColorType::Solid(Color::RGB(0,255,0)),
+                color: ColorType::Solid(Color::RGB(255,255,0)),
             },
         }],
         lights: vec![
         Light {
-            position: Vector::make(1.0, -1.0, -1.0),
+            position: Vector::make(0.0, 0.0, -3.0),
             intensity: 1.0,
         },
     ],
     };
-    //definiramo kamero
+
     let camera = Camera::new(
         Vector {
             x: 0.0,
@@ -58,13 +57,11 @@ fn main() -> Result<(), String> {
         Vector {
             x: 0.0,
             y: 0.0,
-            z: 3.0,
+            z: 1.0,
         },
     );
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    camera.draw(&mut canvas, &scene, WIDTH, RATIO);
+    camera.draw(&mut canvas, &scene, WIDTH, ASPECT_RATIO);
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump()?;
