0c5ca121f428ec94a4703365b3c88625c641c092
Max Rupnik <maxirupnik@gmail.com>
2025-04-16 15:35:13 +0200

    fixed variable names

--- src/camera.rs
+++ src/camera.rs
@@ -8,11 +8,6 @@ pub struct Ray {
     pub direction: Vector,
 }
 
-pub struct Camera {
-    pub coords: Vector,
-    pub direction: Vector,
-}
-
 impl Ray {
     fn new(origin: Vector, direction: Vector) -> Self {
         Ray {
@@ -20,6 +15,13 @@ impl Ray {
             direction: direction.normalized(),
         }
     }
+}
+
+pub struct Camera {
+    ///Location of camera/where all rays shoot
+    pub coords: Vector,
+    /// Direction that camera faces
+    pub direction: Vector,
 }
 
 impl Camera {
@@ -38,20 +40,20 @@ impl Camera {
         ratio: (u16, u16),
     ) {
         let up = Vector::make(0.0, 1.0, 0.0);
-        let forward = self.direction;
-        let right = forward.cross(&up).normalized();
-        let true_up = right.cross(&forward).normalized();
+        let cam_direction = self.direction;
+        let right = cam_direction.cross(&up).normalized();
+        let true_up = right.cross(&cam_direction).normalized();
 
         let (x_ratio, y_ratio) = ratio;
         let height = (width as f64 * (y_ratio as f64 / x_ratio as f64)) as u16;
-        let aspect_ratio = width as f64 / height as f64;
+        let aspect_ratio = x_ratio as f64 / y_ratio as f64;
 
-        for y in 0..height {
-            for x in 0..width {
-                let u = (x as f64 + 0.5) / width as f64 - 0.5;
-                let v = (y as f64 + 0.5) / height as f64 - 0.5;
+        for y_pixel_coordinate in 0..height {
+            for x_pixel_coordinate in 0..width {
+                let u = (x_pixel_coordinate as f64 + 0.5) / width as f64 - 0.5;
+                let v = (y_pixel_coordinate as f64 + 0.5) / height as f64 - 0.5;
 
-                let pixel_dir = forward + right * (u * 2.0 * aspect_ratio) + true_up * (-v * 2.0);
+                let pixel_dir = cam_direction + right * (u * 2.0 * aspect_ratio) + true_up * (-v * 2.0);
 
                 let ray = Ray::new(self.coords, pixel_dir);
 
@@ -76,11 +78,11 @@ impl Camera {
 
                     Color::RGB(r, g, b)
                 } else {
-                    Color::RGB(150, 170, 150 + (y as f64 * 105. / height as f64) as u8)
+                    Color::RGB(150, 170, 150 + (y_pixel_coordinate as f64 * 105. / height as f64) as u8)
                 };
 
                 canvas.set_draw_color(color);
-                let _ = canvas.draw_point(sdl2::rect::Point::new(x as i32, y as i32));
+                let _ = canvas.draw_point(sdl2::rect::Point::new(x_pixel_coordinate as i32, y_pixel_coordinate as i32));
             }
         }
     }
